24db549606aa253fcf6bc518e78292ef0c58efcd
LaiqueMirza <mirzalaique2ey@gmail.com>
2021-04-08 16:09:05 +0530

    git new changes

--- eje/app/src/component/product/products/products.js
+++ eje/app/src/component/product/products/products.js
@@ -44,9 +44,11 @@ const Products = ({data}) => {
         <h4 className="h4-clothing">{data.name}</h4>
         <h5 className="brand-clothing">{data.brand}</h5>
         <h5 className="price-clothing">Rs {data.price}</h5>
-        </div>
-        <h4 className="h4-clothing">{data.name}</h4>
+        <h5 className="price-clothing">Rs {data.price}</h5>
+        <h5 className="price-clothing">Rs {data.price}</h5>
         <h5 className="brand-clothing">{data.brand}</h5>
+        <h5 className="price-clothing">Rs {data.price}</h5>
+        <h5 className="price-clothing">Rs {data.price}</h5>
         <h5 className="price-clothing">Rs {data.price}</h5>
         </div>
       </Link>
